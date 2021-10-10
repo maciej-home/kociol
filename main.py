68760a46a93a62b6a77d44bbebfd4fcbbe25f204
Maciej L <github@maciej.website>
2021-10-10 22:00:41 +0200

    Feature/left in tray (#2)
    * Add 'left in tray'
    
    * Add newline

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ measurement = int(argv[1])
 time_now_datetime = datetime.now()
 time_now_string = time_now_datetime.strftime('%Y-%m-%d %H:%M:%S.%f')
 time_24h_before_now = (time_now_datetime - timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S.%f')
+time_72h_before_now = (time_now_datetime - timedelta(days=3)).strftime('%Y-%m-%d %H:%M:%S.%f')
 current_month_first_day = time_now_datetime.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
 previous_month_last_day = current_month_first_day - timedelta(microseconds=1)
 previous_month_first_day = previous_month_last_day.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+previous_month_available = True
 
 with psycopg.connect(f'dbname={config.db_name} user={config.db_user} password={config.db_pass} host={config.db_host}') as conn:
     with conn.cursor() as cur:
@@ -29,20 +31,28 @@ with psycopg.connect(f'dbname={config.db_name} user={config.db_user} password={c
         ORDER BY measurement_time, measurement_id, measurement LIMIT 1''')
         last_24h_start_impulses = cur.fetchone()[0]
 
+        cur.execute(f'''SELECT measurement FROM test_table WHERE measurement_time >= '{time_72h_before_now}'
+        ORDER BY measurement_time, measurement_id, measurement LIMIT 1''')
+        last_72h_start_impulses = cur.fetchone()[0]
+
         cur.execute(f'''SELECT measurement FROM test_table WHERE measurement_time >= '{previous_month_first_day}'
         ORDER BY measurement_time, measurement_id, measurement LIMIT 1''')
         try:
             previous_month_start_impulses = cur.fetchone()[0]
         except TypeError:
-            previous_month_start_impulses = 0
+            previous_month_available = False
 
         cur.execute(f'''SELECT measurement FROM test_table WHERE measurement_time <= '{previous_month_last_day}'
         ORDER BY measurement_time DESC, measurement_id DESC, measurement DESC LIMIT 1''')
         try:
             previous_month_end_impulses = cur.fetchone()[0]
         except TypeError:
-            previous_month_start_impulses = 0
-            previous_month_end_impulses = 0
+            previous_month_available = False
+
+        cur.execute(f'''SELECT added FROM tray_table WHERE add_time >= '{config.season_start}'
+        ORDER BY add_time, add_id''')
+        added_coal_history = cur.fetchall()
+
 
 ### Calculate used fuel etc. and send to domoticz
 
@@ -50,10 +60,22 @@ season_impulses = measurement - season_start_impulses
 coal_season = season_impulses * config.ratio
 last24h_impulses = measurement - last_24h_start_impulses
 coal_last24h = last24h_impulses * config.ratio
-previous_month_impulses = previous_month_end_impulses - previous_month_start_impulses
-coal_previous_month = previous_month_impulses * config.ratio
+last72h_impulses = measurement - last_72h_start_impulses
+coal_last72h = last72h_impulses * config.ratio
+
+added_coal_sum = 0
+for i in added_coal_history:
+    added_coal_sum += i[0]
+left_in_tray = added_coal_sum - coal_season
+
+if previous_month_available:
+    previous_month_impulses = previous_month_end_impulses - previous_month_start_impulses
+    coal_previous_month = previous_month_impulses * config.ratio
 
 if config.domoticz_enabled:
     requests.get(f'{config.domoticz_url}/json.htm?type=command&param=udevice&idx={config.domoticz_season_idx}&nvalue=0&svalue={round(coal_season, 1)}')
     requests.get(f'{config.domoticz_url}/json.htm?type=command&param=udevice&idx={config.domoticz_last24h_idx}&nvalue=0&svalue={round(coal_last24h, 1)}')
-    requests.get(f'{config.domoticz_url}/json.htm?type=command&param=udevice&idx={config.domoticz_previous_month_idx}&nvalue=0&svalue={round(coal_previous_month, 1)}')
+    requests.get(f'{config.domoticz_url}/json.htm?type=command&param=udevice&idx={config.domoticz_left_in_tray_idx}&nvalue=0&svalue={round(left_in_tray, 1)}')
+    if previous_month_available:
+        # noinspection PyUnboundLocalVariable
+        requests.get(f'{config.domoticz_url}/json.htm?type=command&param=udevice&idx={config.domoticz_previous_month_idx}&nvalue=0&svalue={round(coal_previous_month, 1)}')
